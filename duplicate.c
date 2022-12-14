b863459759ac1ea1e0c298dde59b42c079b73713
Iyanu Sam <iyanuxn@gmail.com>
2022-12-14 14:09:27 +0100

    Add files via upload

--- duplicate.c
+++ duplicate.c
@@ -1,120 +1,74 @@
-# include <stdio.h>
-# include <string.h>
-# include <stdlib.h>
+// DUPLICATE FILE FINDER
+// COURSE CODE: CSC 235
+// NAME: OLUWADAMILARE IYANUOLUWA SAMUEL
+// MATRIC NUMBER: 222505
 
-// function to find least in array
-int least(int arr[], int n)
-{
-    int i, min = arr[0];
-    for (i = 1; i < n; i++)
-        if (arr[i] < min)
-            min = arr[i];
-    return min;
-}
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// Declaring all the variables that will be used in the program.
+int file_check = 0;
+int number_of_files;
+int store = 0;
+int lowest_value = 0;
+char name_of_file[100][50];
+int file_id[100];
 
 
+// It compares the file names and if they are the same, it compares each file ID and if the file ID of
+// the first file is greater than the file ID of the second file, it assigns the file ID of the second
+// file to the lowest_value variable.
 
-int main()
-{
-    int num;
-    char file[100], id[100];
-    int counter = 0;
-    int trials = 0;
-    FILE * fpointer = fopen("file.in.txt", "r");
-    FILE * buffer = fopen ("standard_output.txt", "w");
-    /* Reading the first two lines of the file. */
-    fscanf(fpointer, "%d", &num);
-    fscanf(fpointer, "%d", &num);
-    char num_files[3][50];
-    char total_id[3][50];
-
-    while (trials != 2)
-    {
-        while (num != counter)
-        {    
-        /* Reading the file and storing the values in the array. */
-        fscanf(fpointer, "%s", file);    
-        strcpy(num_files[counter], file);
-
-        fscanf(fpointer, "%s", id);
-        strcpy(total_id[counter], id);
-        
-        counter++;
-        }
-        counter = 0;
-
-        int total_id_int[3];
-        /* This is converting the string values in the array to integers. */
-        for (int i = 0; i < 3; i++)
-        {
-            total_id_int[i] = atoi(total_id[i]);
-        }
-        int len_id = sizeof(total_id_int)/sizeof(total_id_int[0]);
-        int len_file = sizeof(num_files)/sizeof(num_files[0]);
-        int valid = 0;
-        
-        //sort the total_id_int array
-        for (int i = 0; i < len_id -1; i++)
-        {
-            for (int j = i + 1; j < len_id - 1; j++)
-            {
-                if (total_id_int[i] > total_id_int[j])
-                {
-                    int temp = total_id_int[i];
-                    total_id_int[i] = total_id_int[j];
-                    total_id_int[j] = temp;
-                }
+void duplicate_checker(void){
+    for (int j = 0; j< number_of_files; j++) {
+        for (int i = j + 1; i < number_of_files; i++) {
+            file_check = strcmp(name_of_file[j], name_of_file[i]);
+            // if file_check returns 0 then they are equal
+            if(file_check == 0 &&  (file_id[j] > file_id[i])){
+                lowest_value = file_id[i];
+            }
+            else if(file_check != 0 && (file_id[j] > file_id[i])){
+                store = file_id[j];
+                file_id[j] = file_id[i];
+                file_id[i] = store;
             }
         }
-
-        // compare all values in num_files    
-        for (int i = 0; i < len_file - 1; i++)
-        {
-            for (int j = i + 1; j < len_file - 1; j++)
-            {
-                if (strcmp(num_files[i], num_files[j]) == 0)
-                {
-                    printf("\n"); 
-                }
-                else
-                { 
-                    for (int i = 0; i < len_id - 1; i++)
-                    {
-                        printf("%d ", total_id_int[i]);
-                       /* Writing the values in the array to the file. */
-                        fprintf(buffer, "%d ", total_id_int[i]);
-                        
-                        valid = 1;
-                    }
-
-                }
-            }
-        }
-        
-        //if all the values are the same, then print the least id   
-         
-        if (least(total_id_int, len_id - 1 ) == total_id_int[0])
-        {
-            if (valid == 1)
-            {
-                valid = 0;   
-            }
-            else
-            {
-                printf("%d\n", least(total_id_int, len_id ));
-                fprintf(buffer, "\n");
-                fprintf(buffer, "%d\n", least(total_id_int, len_id ));
-            }
-            
-        }
-    trials++;
-    fscanf(fpointer, "%d", &num);
     }
-    fclose(fpointer);
-    fclose(buffer);
-    return 0;
 }
 
 
+int main(){
+    int trials;
+    FILE * file = fopen("file.in.txt", "r");
 
-    
+    fscanf(file, "%d", &trials);
+
+    // A for loop that is used to iterate through the number of trials.
+    for (int i = 0; i< trials; i++) {
+
+        // Reading the number of files from the file.
+        fscanf(file, "%d",&number_of_files);
+        int j = 0;
+       
+        // A while loop to go through the number of files.
+        while (j<number_of_files) {
+            fscanf(file,"%s %d", name_of_file[j], &file_id[j]);
+            j++;
+        }
+        // Calling the duplicate_checker function.
+        duplicate_checker();
+
+        for (int j = 0; j< number_of_files; j++) {
+            if (file_check != 0){
+                printf("%d ", file_id[j]);
+            }
+        }
+        printf("\n");
+        // Checking if the files are the same
+        if (file_check == 0){
+            printf("%d\n", lowest_value);
+        }
+    }
+    fclose(file);
+}
